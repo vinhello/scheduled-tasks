2aef79fa7f2026697795291c88b4642fc2f7cd2f
vinhello <voongvincent@gmail.com>
2026-06-08 20:14:10 -0700

    Improve birthday email functionality and error checks
    Refactor birthday email sending logic and error handling.

--- main.py
+++ main.py
@@ -6,33 +6,62 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
 import smtplib
+import datetime as dt
+import random
+import pandas
+import sys
 import os
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# get today's date
+now = dt.datetime.now()
+today_month = now.month
+today_day = now.day
+today = (today_month, today_day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+# read birthdays from csv file using Pandas
+try:
+    data_frame = pandas.read_csv("birthdays.csv")
+except FileNotFoundError:
+    print("File not found.")
+    sys.exit(1)
+else:
+    birthdays_dict = {(row.month, row.day): row for (index, row) in data_frame.iterrows()}
+    print(birthdays_dict)
+
+# match today's date with any birthdays in birthdays_dict
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    person_name = birthday_person["name"]
+    person_email = birthday_person["email"]
+
+    letter_num = random.randint(1, 3)
+    try:
+        with open(f"letter_templates/letter_{letter_num}.txt", "r") as data:
+            letter = data.read()
+    except FileNotFoundError:
+        print("File not found.")
+        sys.exit(1)
+
+    # replace [NAME] with name; using .replace() must be saved to a new variable
+    updated_letter = letter.replace("[NAME]", person_name)
+
+    # send email
+    try:
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=f"{person_email}",
+                msg=f"Subject: Happy Birthday!\n\n{updated_letter}"
+            )
+    except Exception as e:
+        print(f"Failed to send email. Error: {e}")
+    else:
+        print("No birthday's match today's date.")
